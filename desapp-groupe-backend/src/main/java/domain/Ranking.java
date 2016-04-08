f59444cc3d2c29c540dcfdeca43ca5e7dd1877de
trimegisto <federico.lochbaum@gmail.com>
2016-04-08 12:34:51 -0300

    calculateWorstDrives en System

--- desapp-groupe-backend/src/main/java/domain/Ranking.java
+++ desapp-groupe-backend/src/main/java/domain/Ranking.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,19 +14,31 @@ public class Ranking {
 
     public Ranking(Integer month,Integer year,List<User> users){
         bestDrivers = calculateBestDrivers(users,month,year);
-        //worstDrivers = calculateWorstDrivers(users,month,year);
+        worstDrivers = calculateWorstDrivers(users,month,year);
         //bestPassenger = calculateBestPassenger(users,month,year);
         //worstPassenger = calculateWorstPassenger(users,month,year);
         //bestVehicles = calculateBestVehicles(users,month,year);
         //worstVehicles = calculateWorstVehicles(users,month,year);
     }
 
-    private List<User> calculateBestDrivers(List<User> users, Integer month, Integer year) {
-        List<User> drivers = users.stream().filter(user -> user.isDriver()).collect(Collectors.toList());
-        return orderUsersbyPointMaxToMin(users).subList(0,19);
+    private List<User> driversIn(List<User> users){
+        return users.stream().filter(user -> user.isDriver()).collect(Collectors.toList());
     }
 
+    private List<User> calculateWorstDrivers(List<User> users, Integer month, Integer year) {
+        return orderUsersbyPointMinToMax(driversIn(users)).subList(0,19);
+    }
+
+    private List<User> calculateBestDrivers(List<User> users, Integer month, Integer year) {
+        return orderUsersbyPointMaxToMin(driversIn(users)).subList(0,19);
+    }
+
+    //TODO : revisar, no estoy seguro de que cambiando el orden cambia el orden del resultado
     private List<User> orderUsersbyPointMaxToMin(List<User> users) {
+        return users.stream().sorted((user1,user2) -> Integer.compare(user2.getPoints(),user1.getPoints())).collect(Collectors.toList());
+    }
+
+    private List<User> orderUsersbyPointMinToMax(List<User> users) {
         return users.stream().sorted((user1,user2) -> Integer.compare(user1.getPoints(),user2.getPoints())).collect(Collectors.toList());
     }
 
