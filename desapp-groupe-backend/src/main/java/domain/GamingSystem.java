d7692405fcde0026c75ac039a2b2972f4cedab5e
danwyry <danwyry@gmail.com>
2016-04-10 15:32:15 -0300

    Eliminamos Reputation, ReputationSystem.

--- desapp-groupe-backend/src/main/java/domain/GamingSystem.java
+++ desapp-groupe-backend/src/main/java/domain/GamingSystem.java
@@ -3,21 +3,15 @@ public class GamingSystem {
     private ScoringSystem scoringSystem;
     private ProductsSystem productsSystem;
     private System system;
-    private ReputationSystem reputationSystem;
 
     public GamingSystem(System system){
         this.system = system;
         scoringSystem = new ScoringSystem();
         productsSystem = new ProductsSystem();
-        reputationSystem = new ReputationSystem();
     }
 
     public ProductsSystem getProductsSystem() {
         return productsSystem;
-    }
-
-    public ReputationSystem getReputationSystem() {
-        return reputationSystem;
     }
 
     public ScoringSystem getScoringSystem() {
