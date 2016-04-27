aefa9f52710a4f0bd9f656a283aca444bfb64f87
trimegisto <federico.lochbaum@gmail.com>
2016-04-27 18:54:39 -0300

    bean de services de User

---
+++ desapp-groupe-backend/src/main/java/domain/servicesRest/GeneralService.java
@@ -0,0 +1,14 @@
+package domain.servicesRest;
+
+public class GeneralService {
+
+    private UserService userService;
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
