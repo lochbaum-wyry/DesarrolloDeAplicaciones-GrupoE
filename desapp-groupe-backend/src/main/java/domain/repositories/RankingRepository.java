044a07adb76dc880dbf9ebfd4e7082232a798dfd
trimegisto <federico.lochbaum@gmail.com>
2016-05-03 11:13:44 -0300

    Se agrego la persistencia en createRanking de GamingService

--- desapp-groupe-backend/src/main/java/domain/repositories/RankingRepository.java
+++ desapp-groupe-backend/src/main/java/domain/repositories/RankingRepository.java
@@ -1,6 +1,7 @@
 package domain.repositories;
 
 import domain.services.MonthlyRanking;
+import org.hibernate.Query;
 
 public class RankingRepository extends HibernateGenericDao<MonthlyRanking> implements
         GenericRepository<MonthlyRanking> {
@@ -10,5 +11,17 @@ public class RankingRepository extends HibernateGenericDao<MonthlyRanking> imple
     @Override
     protected Class<MonthlyRanking> getDomainClass() {
         return MonthlyRanking.class;
+    }
+
+    public MonthlyRanking getRankingIn(Integer month,Integer year){
+        String hql = " SELECT r FROM " +  MonthlyRanking.class.getName() +
+                " AS r WHERE r.month = :month AND r.year = :year";
+
+        Query query =  getHibernateTemplate().getSessionFactory().getCurrentSession().createQuery(hql);
+        query.setParameter("month",month);
+        query.setParameter("year",year);
+
+        MonthlyRanking ranking = (MonthlyRanking) query.uniqueResult();
+        return ranking;
     }
 }
--- desapp-groupe-backend/src/main/java/domain/services/gaming_service/GamingService.java
+++ desapp-groupe-backend/src/main/java/domain/services/gaming_service/GamingService.java
@@ -1,5 +1,6 @@
 package domain.services.gaming_service;
 
+import domain.repositories.RankingRepository;
 import domain.repositories.UserRepository;
 import domain.repositories.VehicleRepository;
 import domain.services.System;
@@ -11,23 +12,29 @@ public class GamingService
 {
     private UserRepository userRepository ;
     private VehicleRepository vehicleRepository;
+    private RankingRepository rankingRepository;
 
-    public GamingService(UserRepository userRepository, VehicleRepository vehicleRepository)
+    public GamingService(UserRepository userRepository, VehicleRepository vehicleRepository,RankingRepository rankingRepository)
     {
         this.userRepository = userRepository ;
         this.vehicleRepository = vehicleRepository ;
+        this.rankingRepository = rankingRepository;
     }
 
     public MonthlyRanking createRanking(Integer month, Integer year)
     {
-        MonthlyRanking ranking = new MonthlyRanking(month,year);
-        ranking.setBestDrivers(userRepository.getBestDriversInMonthYear(month, year,20));
-        ranking.setWorstDrivers(userRepository.getWorstDriversInMonthYear(month,year,20));
-        ranking.setBestPassenger(userRepository.getBestPassengersInMonthYear(month,year,20));
-        ranking.setWorstPassenger(userRepository.getWorstPassengersInMonthYear(month,year,20));
-        ranking.setBestVehicles(vehicleRepository.getBestVehiclesInMonthYear(month,year,20));
-        ranking.setWorstVehicles(vehicleRepository.getWorstVehiclesInMonthYear(month,year,20));
-        ranking.setMostEfficientDrivers(userRepository.getMostEfficientDriversInMonthYear(month, year));
+        MonthlyRanking ranking = rankingRepository.getRankingIn(month,year);
+        if(ranking == null) {
+            ranking = new MonthlyRanking(month, year);
+            ranking.setBestDrivers(userRepository.getBestDriversInMonthYear(month, year, 20));
+            ranking.setWorstDrivers(userRepository.getWorstDriversInMonthYear(month, year, 20));
+            ranking.setBestPassenger(userRepository.getBestPassengersInMonthYear(month, year, 20));
+            ranking.setWorstPassenger(userRepository.getWorstPassengersInMonthYear(month, year, 20));
+            ranking.setBestVehicles(vehicleRepository.getBestVehiclesInMonthYear(month, year, 20));
+            ranking.setWorstVehicles(vehicleRepository.getWorstVehiclesInMonthYear(month, year, 20));
+            ranking.setMostEfficientDrivers(userRepository.getMostEfficientDriversInMonthYear(month, year));
+            rankingRepository.save(ranking);
+        }
         return ranking ;
     }
 }
