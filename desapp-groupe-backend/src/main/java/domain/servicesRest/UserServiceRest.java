0a1a32584438b59c7f5037db1605ad3ddbf3fc03
trimegisto <federico.lochbaum@gmail.com>
2016-05-04 01:58:13 -0300

    Agregado de configuracion y creacion de UserServiceRest

---
+++ desapp-groupe-backend/src/main/java/domain/servicesRest/UserServiceRest.java
@@ -0,0 +1,26 @@
+package domain.servicesRest;
+
+import domain.User;
+import domain.services.UserService;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/user")
+public class UserServiceRest {
+
+    UserService userService;
+
+    public UserServiceRest(UserService userService) {
+        this.userService = userService;
+    }
+
+    @POST
+    @Path("/{from}")
+    @Produces("application/json")
+    public User singUp(){
+        return null;
+    }
+
+}
