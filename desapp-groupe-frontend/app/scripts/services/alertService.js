edd0542196ef277299d49370d659949359386320
trimegisto <federico.lochbaum@gmail.com>
2016-07-10 19:09:39 -0300

    Todo el Responsive del sitio

--- desapp-groupe-frontend/app/scripts/services/alertService.js
+++ desapp-groupe-frontend/app/scripts/services/alertService.js
@@ -10,12 +10,12 @@ function AlertService(Flash) {
   };
 
   function successAlert(msg) {
-        var message = '<strong>Bien! </strong>' + msg;
+        var message = '<strong></strong>' + msg;
         var id = Flash.create('success', message, 2000, {class: 'custom-class', id: 'custom-id'}, true);
   };
 
   function failureAlert(error) {
-        var message = '<strong>Mal! </strong> ' + error;
+        var message = '<strong></strong> ' + error;
         var id = Flash.create('danger', message, 2000, {class: 'custom-class', id: 'custom-id'}, true);
   };
 
